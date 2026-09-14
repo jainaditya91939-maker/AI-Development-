72cb843826eb70235fa7a3933a206d1ec428575b
jainaditya91939-maker <jainaditya1939@gmail.com>
2026-09-14 23:47:44 +0530

    Allow Vercel frontend in AI CORS

--- main.py
+++ main.py
@@ -16,10 +16,14 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://192.168.0.106:5173"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://192.168.0.106:5173",
+    "https://ai-business-investigator-frontend.vercel.app",
+],
+
+    
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
